5ab51c26a03eae7b62706307a8e77fc91a8b29b6
Gustavo-Ennes <gustavo.ennes@3778.care>
2024-08-27 10:03:22 -0300

    Config module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TenantModule } from './tenant/tenant.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'sa',
-      password: '1Senha!.',
-      database: 'realEstate',
-      autoLoadModels: true,
-      synchronize: true,
-    }),
-    TenantModule,
-  ],
+  imports: [ConfigModule, TenantModule],
 })
 export class AppModule {}
---
+++ src/config/config.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'sa',
+      password: '1Senha!.',
+      database: 'realEstate',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class ConfigModule {}
